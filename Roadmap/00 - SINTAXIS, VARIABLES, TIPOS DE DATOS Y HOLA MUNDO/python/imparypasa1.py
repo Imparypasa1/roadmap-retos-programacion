1ab0cb45d33a07a08f370576849f5695b52c7b4f
Imparypasa1 <78589686+Imparypasa1@users.noreply.github.com>
2024-04-22 20:46:52 +0200

    nuevo cambio

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/imparypasa1.py
@@ -0,0 +1,20 @@
+
+#https://www.python.org/
+
+#comentario en una linea
+"""
+esto es un comenatrio 
+en varias lineas de python
+"""
+
+'''
+esto es un comenatrio 
+en varias lineas de python
+'''
+
+my_Variable = "esto es una prueba"
+my_Variable = "valor de prueba"
+
+MY_CONSTANT = "Mi constante"
+
+
